ec0901162ec241d865b14a23f67a5e623ccaf676
kaguyatheprogrammer <bamboo.kingdum@gmail.com>
2026-08-15 15:36:42 +0000

    server 構築

---
+++ server/controllers/authController.js
@@ -0,0 +1,34 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const login = async (req, res) => {
+    // Login logic here
+    try {
+        // Your login logic here
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if(!user) {
+            res.status(404).json({success: false, error: "User Not Found"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            res.status(404).json({success: false, error: "Wrong Password"})
+        }
+
+        const token = jwt.sign({_id: user._id, role: user.role},
+            process.env.JWT_KEY, {expiresIn: "10d"}
+        )
+        res.status(200).json({success: true, token, user: {
+            _id: user._id,
+            name: user.name,
+            role: user.role,
+        }});
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export { login };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import authRouter from './routes/auth.js';
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api/auth', authRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is Running on port ${process.env.PORT}`)
